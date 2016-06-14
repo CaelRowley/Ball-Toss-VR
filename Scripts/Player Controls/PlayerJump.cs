f1e88e5c896bc3cfba77fa10e830e117b762c895
CaelRowley <caelrowley@gmail.com>
2016-06-14 18:01:42 +0100

    Ball Toss VR Initial State
    This is the Initial Stage of the Project

---
+++ Scripts/Player Controls/PlayerJump.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    public float jumpSpeed;
+    public float jumpUpdateTime;
+    public float jumpFilterStrength;
+    public float jumpShakeLimit;
+    public bool canJump = true;
+
+    private float jumpMinShakeFilter;
+    private Vector3 currentAcceleration = Vector3.zero;
+    private Vector3 startAcceleration;
+    private Vector3 shake;
+
+    public AudioClip audioClipJump;
+    private AudioSource audioSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        jumpMinShakeFilter = jumpUpdateTime / jumpFilterStrength;
+
+        // Creates audio source for player
+        GameObject child = new GameObject("Player");
+        child.transform.parent = gameObject.transform;
+        audioSource = child.AddComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(canJump)
+        {
+            Jump();
+            canJump = false;
+        }
+    }
+
+    private void Jump()
+    {
+        // Finds the current shake of the accelerometer  
+        startAcceleration = Input.acceleration;
+        currentAcceleration = Vector3.Lerp(currentAcceleration, startAcceleration, jumpMinShakeFilter);
+        shake = startAcceleration - currentAcceleration;
+
+        // The player jumps when the shake goes over the shake limit
+        if(shake.sqrMagnitude >= jumpShakeLimit)
+        {
+            audioSource.PlayOneShot(audioClipJump);
+            transform.Translate(Vector3.up * jumpSpeed * Time.deltaTime, Space.World);
+        }
+
+        // The player jumps when space is pressed
+        if(Input.GetKeyDown("space"))
+        {
+            audioSource.PlayOneShot(audioClipJump);
+            transform.Translate(Vector3.up * jumpSpeed * 4 * Time.deltaTime * 3, Space.World);
+        }
+    }
+}
---
+++ Scripts/Player Controls/PlayerThrow.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class PlayerThrow : MonoBehaviour
+{
+    public Rigidbody projectile;
+    public float throwDistanceMax;
+    public int projectileCount;
+    public float throwMagnitude;
+
+    private bool canThrow = true;
+    private Vector3 targetPosition;
+
+    private void Update()
+    {
+        if(projectileCount <= 0)
+            canThrow = false;
+
+        RaycastHit objectHit;
+        targetPosition = transform.forward * throwDistanceMax;
+
+        if(Physics.Raycast(transform.position, transform.forward, out objectHit, throwDistanceMax))
+        {
+            targetPosition = objectHit.point;
+            Debug.DrawLine(targetPosition, transform.position, Color.red, 1.0f, false);
+        }
+
+        if(GvrViewer.Instance.Triggered)
+        {
+            if(canThrow)
+            {
+                //Vector3 velocity = CalculateVelocity(targetPosition);
+                CalculateVelocity(targetPosition);
+            }
+            else
+            {
+                Debug.Log("Out of projectiles.");
+            }
+        }
+    }
+
+    private void CalculateVelocity(Vector3 targetPosition)
+    {
+        Vector3 targetDirection = targetPosition - transform.position; // get target direction
+        float directionHeight = targetDirection.y;  // get height difference
+        targetDirection.y = 0;  // retain only the horizontal direction
+        float distanceToTarget = targetDirection.magnitude;  // get horizontal distance
+        targetDirection.y = distanceToTarget;  // set elevation to 45 degrees
+        distanceToTarget += directionHeight;  // correct for different heights
+
+        float velocity = Mathf.Sqrt(distanceToTarget * Physics.gravity.magnitude * throwMagnitude);
+
+        Rigidbody instantiatedProjectile = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
+        instantiatedProjectile.velocity = velocity * targetDirection.normalized;
+
+        //Debug.Log("Distance to target: " + distanceToTarget);
+    }
+}
