2d088c5789642772f4f594c5845445fc02408142
PaddyMc <10109781@studentmail.ul.ie>
2016-06-15 15:52:33 +0100

    Exit function fix
    Added:
    - Changed OnCollision Enter

--- Scripts/Exit/ExitDoorScript.cs
+++ Scripts/Exit/ExitDoorScript.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class ExitDoorScript : MonoBehaviour {
 
-    private void onCollisionEnter(Collision colllider)
+    private void OnCollisionEnter(Collision colllider)
     {
-        Debug.Log("Collision");
+        Debug.Log("Exit Collision");
         //Application.Quit();
+        //SceneManager.LoadScene("Level 1 Office");
     }
 }
