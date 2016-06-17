11c697ea0da387fa9d473be59cbefa08f68952b5
PaddyMc <10109781@studentmail.ul.ie>
2016-06-17 15:20:08 +0100

    Updated transition between stages + throw
    Added:
    - cup counting
    - room transition
    - edited throw

---
+++ Scripts/Floor Removal/AddRoom.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddRoom : MonoBehaviour {
+    public string roofName = "Roof Stage 2";
+    GameObject roof;
+    void Start()
+    {
+        roof = GameObject.Find(roofName);
+    }    
+    private void OnCollisionEnter(Collision collider)
+    {        
+        roof.SetActive(true);
+    }
+}
