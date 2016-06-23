dfbad0548168ca0325ff40c3c96a668e0190dc5c
CaelRowley <caelrowley@gmail.com>
2016-06-23 13:33:54 +0100

    Re Sync

---
+++ Scripts/Text/ButtonTextScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonTextScript : MonoBehaviour
+{
+    public string textToDisplay;
+    public GameObject button;
+
+    // Use this for initialization
+    void Start()
+    {
+        TextMesh textMesh = (TextMesh)button.GetComponent("TextMesh");
+        textMesh.text = textToDisplay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
